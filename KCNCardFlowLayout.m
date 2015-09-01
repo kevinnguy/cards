d6b2e75e44a9a9c5d199bc72aef3f8e23df50d81
Kevin Nguy <kevnguy@gmail.com>
2015-09-01 14:43:37 -0700

    Added cell animation when scrolling

--- KCNCardFlowLayout.m
+++ KCNCardFlowLayout.m
@@ -13,6 +13,11 @@
 @property (nonatomic, strong) NSMutableArray *deleteIndexPaths;
 @property (nonatomic, strong) NSMutableArray *insertIndexPaths;
 
+// Cell animation
+@property (nonatomic, strong) UIDynamicAnimator *dynamicAnimator;
+@property (nonatomic, strong) NSMutableSet *visibleIndexPathsSet;
+@property (nonatomic, assign) CGFloat latestDelta;
+
 @end
 
 @implementation KCNCardFlowLayout
@@ -37,7 +42,7 @@
     NSInteger itemHeight = 250;
     
     self.minimumInteritemSpacing = 10.0f;
-    self.minimumLineSpacing = 4.0f;
+    self.minimumLineSpacing = 6.0f;
     
     self.itemSize = CGSizeMake(itemWidth, itemHeight);
     self.sectionInset = UIEdgeInsetsMake(CGRectGetHeight(frame) - itemHeight - self.minimumLineSpacing,
@@ -47,10 +52,102 @@
 
     self.scrollDirection = UICollectionViewScrollDirectionHorizontal;
     
+    self.dynamicAnimator = [[UIDynamicAnimator alloc] initWithCollectionViewLayout:self];
+    self.visibleIndexPathsSet = [NSMutableSet set];
+    
     return self;
 }
 
+- (void)prepareLayout {
+    [super prepareLayout];
+    
+    // Need to overflow our actual visible rect slightly to avoid flickering.
+    CGRect visibleRect = CGRectInset((CGRect){.origin = self.collectionView.bounds.origin, .size = self.collectionView.frame.size}, -200, -200);
+    
+    NSArray *itemsInVisibleRectArray = [super layoutAttributesForElementsInRect:visibleRect];
+    
+    NSSet *itemsIndexPathsInVisibleRectSet = [NSSet setWithArray:[itemsInVisibleRectArray valueForKey:@"indexPath"]];
+    
+    // Step 1: Remove any behaviours that are no longer visible.
+    NSArray *noLongerVisibleBehaviours = [self.dynamicAnimator.behaviors filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(UIAttachmentBehavior *behaviour, NSDictionary *bindings) {
+        BOOL currentlyVisible = [itemsIndexPathsInVisibleRectSet member:[[[behaviour items] firstObject] indexPath]] != nil;
+        return !currentlyVisible;
+    }]];
+    
+    [noLongerVisibleBehaviours enumerateObjectsUsingBlock:^(id obj, NSUInteger index, BOOL *stop) {
+        [self.dynamicAnimator removeBehavior:obj];
+        [self.visibleIndexPathsSet removeObject:[[[obj items] firstObject] indexPath]];
+    }];
+    
+    // Step 2: Add any newly visible behaviours.
+    // A "newly visible" item is one that is in the itemsInVisibleRect(Set|Array) but not in the visibleIndexPathsSet
+    NSArray *newlyVisibleItems = [itemsInVisibleRectArray filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(UICollectionViewLayoutAttributes *item, NSDictionary *bindings) {
+        BOOL currentlyVisible = [self.visibleIndexPathsSet member:item.indexPath] != nil;
+        return !currentlyVisible;
+    }]];
+    
+    CGPoint touchLocation = [self.collectionView.panGestureRecognizer locationInView:self.collectionView];
+    
+    [newlyVisibleItems enumerateObjectsUsingBlock:^(UICollectionViewLayoutAttributes *item, NSUInteger idx, BOOL *stop) {
+        CGPoint center = item.center;
+        UIAttachmentBehavior *springBehaviour = [[UIAttachmentBehavior alloc] initWithItem:item attachedToAnchor:center];
+        
+        springBehaviour.length = 0.0f;
+        springBehaviour.damping = 0.8f; // how bouncy: 0 is bounciest, 1 is no bounce
+        springBehaviour.frequency = 1.0f; // speed: 0 is slowest, 1 is normal
+        
+        // If our touchLocation is not (0,0), we'll need to adjust our item's center "in flight"
+        if (!CGPointEqualToPoint(CGPointZero, touchLocation)) {
+            CGFloat yDistanceFromTouch = fabs(touchLocation.y - springBehaviour.anchorPoint.y);
+            CGFloat xDistanceFromTouch = fabs(touchLocation.x - springBehaviour.anchorPoint.x);
+            CGFloat scrollResistance = (yDistanceFromTouch + xDistanceFromTouch) / 1500.0f;
+            
+            if (self.latestDelta < 0) {
+                center.x += MAX(self.latestDelta, self.latestDelta*scrollResistance);
+            } else {
+                center.x += MIN(self.latestDelta, self.latestDelta*scrollResistance);
+            }
+            item.center = center;
+        }
+        
+        [self.dynamicAnimator addBehavior:springBehaviour];
+        [self.visibleIndexPathsSet addObject:item.indexPath];
+    }];
+}
+
+- (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect {
+    return [self.dynamicAnimator itemsInRect:rect];
+}
+
+- (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return [self.dynamicAnimator layoutAttributesForCellAtIndexPath:indexPath];
+}
+
 - (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)oldBounds {
+    UIScrollView *scrollView = self.collectionView;
+    CGFloat delta = oldBounds.origin.x - scrollView.bounds.origin.x;
+    
+    self.latestDelta = delta;
+    
+    CGPoint touchLocation = [self.collectionView.panGestureRecognizer locationInView:self.collectionView];
+    
+    [self.dynamicAnimator.behaviors enumerateObjectsUsingBlock:^(UIAttachmentBehavior *springBehaviour, NSUInteger idx, BOOL *stop) {
+        CGFloat yDistanceFromTouch = fabs(touchLocation.y - springBehaviour.anchorPoint.y);
+        CGFloat xDistanceFromTouch = fabs(touchLocation.x - springBehaviour.anchorPoint.x);
+        CGFloat scrollResistance = (yDistanceFromTouch + xDistanceFromTouch) / 2500.0f;
+        
+        UICollectionViewLayoutAttributes *item = [springBehaviour.items firstObject];
+        CGPoint center = item.center;
+        if (delta < 0) {
+            center.x += MAX(delta, delta*scrollResistance);
+        } else {
+            center.x += MIN(delta, delta*scrollResistance);
+        }
+        item.center = center;
+        
+        [self.dynamicAnimator updateItemUsingCurrentState:item];
+    }];
+    
     return NO;
 }
 
@@ -78,45 +175,26 @@
     self.insertIndexPaths = nil;
 }
 
-// Note: name of method changed
-// Also this gets called for all visible cells (not just the inserted ones) and
-// even gets called when deleting cells!
 - (UICollectionViewLayoutAttributes *)initialLayoutAttributesForAppearingItemAtIndexPath:(NSIndexPath *)itemIndexPath {
     UICollectionViewLayoutAttributes *attributes = [super initialLayoutAttributesForAppearingItemAtIndexPath:itemIndexPath];
     if ([self.insertIndexPaths containsObject:itemIndexPath]) {
-        // only change attributes on inserted cells
         if (!attributes) {
             attributes = [self layoutAttributesForItemAtIndexPath:itemIndexPath];
         }
         
-        NSLog(@"asdf");
-
-        // Configure attributes ...
         attributes.transform = CGAffineTransformScale(CGAffineTransformMakeScale(0.01f, 0.01f), 1, 1);
-//        attributes.transform3D = CATransform3DMakeScale(0.0, 0.0, 1);
     }
     
     return attributes;
 }
 
-// Note: name of method changed
-// Also this gets called for all visible cells (not just the deleted ones) and
-// even gets called when inserting cells!
 - (UICollectionViewLayoutAttributes *)finalLayoutAttributesForDisappearingItemAtIndexPath:(NSIndexPath *)itemIndexPath {
     UICollectionViewLayoutAttributes *attributes = [super finalLayoutAttributesForDisappearingItemAtIndexPath:itemIndexPath];
     if ([self.deleteIndexPaths containsObject:itemIndexPath]) {
-        // only change attributes on deleted cells
         if (!attributes) {
             attributes = [self layoutAttributesForItemAtIndexPath:itemIndexPath];
         }
-        
-        NSLog(@"dsfsfd");
 
-        // Configure attributes ...
-//        attributes.transform = CGAffineTransformScale(attributes.transform, 0, 0);
-//        attributes.transform = CGAffineTransformScale(CGAffineTransformMakeScale(0, 0), 0, 0);
-//        attributes.transform3D = CATransform3DMakeScale(0.9, 0.9, 0.5);
-//        attributes.transform = CGAffineTransformMakeScale(0.01f, 0.01f);
         attributes.transform = CGAffineTransformScale(CGAffineTransformMakeScale(0.01f, 0.01f), 1, 1);
     }
     
